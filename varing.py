d32c1fa7b8bcef1ac1e22e489b1c6571091d76e9
erfaw <146765011+erfaw@users.noreply.github.com>
2025-10-08 11:04:57 +0330

    in 'varing.py': 1. use 'stringer.en_formated_text_output()' to make a file of good structured text of classes and exam, 2. delete some comment code, not important,

--- varing.py
+++ varing.py
@@ -6,11 +6,8 @@ stringer = Stringer()
 
 stringer.raw_data = stringer.load_raw_data('./one of classes.txt')
 stringer.formated_str = stringer.format_converter(stringer.raw_data)
-# print(stringer.formated_str)
-# print(stringer.split_with_comma(stringer.formated_str))
 
-for index in stringer.split_with_comma(stringer.formated_str):
-    print(index)
+stringer.en_formated_text_output()
 
 
 
