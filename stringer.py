9e69534ae8b6b52c22f3b88d3cd40b7b075d70e5
erfaw <146765011+erfaw@users.noreply.github.com>
2025-10-08 10:40:59 +0330

    in 'stringer.py': 1. add method 'format_converter()', 2. run-check 'fomat_converter()' done, 3. add method 'split_with_comma()', (NOT CHECK YET)

--- stringer.py
+++ stringer.py
@@ -2,8 +2,17 @@ class Stringer(str):
     def __init__(self):
         super().__init__()
         self.raw_data = None
+        self.formated_str = None
 
     def load_raw_data(self, address):
         """opens txt file of date details in not formated string from base file, address is file path"""
         with open(address, mode='r') as file:
             return file.read()
+    
+    def format_converter(self, string):
+        """catch a not formated string and replace letters with what we want. and return it"""
+        return string.replace(r"/", '-').replace(r"،", ',').replace(r"۱",'1').replace(r"۲",'2').replace(r"۳",'3').replace(r"۴",'4').replace(r"۵",'5').replace(r"۶",'6').replace(r"۷",'7').replace(r"۸",'8').replace(r"۹",'9').replace(r"۹",'9').replace(r"۰",'0').replace(r")",')').replace(r"(",'(').replace(r"(",'(').replace(r"-",'-').replace(r"امتحان",'EXAM').replace(r"ساعت",'time').replace(r"درس(ت)", 'class').replace(r":", ':').strip()
+
+    def split_with_comma(self, string):
+        """catch a string, split it with ',' and return it"""
+        return string.split(',')
